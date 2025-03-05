c8ab30af2c0d7bf0b53efb0521d70d5944e39e0d
amelabrigid <121575401+amelabrigid@users.noreply.github.com>
2025-03-05 13:28:05 +0300

    Add files via upload

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,36 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "stack.h"
+using namespace std;
+
+class Arithmetic
+{
+	string infix;
+	string postfix;
+	string operands;
+	string* variables;
+	string* SetOfLexems;
+	int lexemSize;
+	int variableSize;
+
+public:
+	~Arithmetic();
+	Arithmetic(string& str);
+	void setInfix(string& str);
+	string getInfix() const;
+	string getPostfix() const;
+	string getOperands() const;
+	bool isOperation(const char& x);
+	int priority(const char& operation);
+	void makePostfix();
+	double makeDouble(string number);
+	void Parser();
+	void infixCheck();
+	bool isOperand(const char& first_symbol);
+	bool isDigit(const char& first_symbol);
+	double calculate();
+};
--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,88 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыдел€тьс€ пам€ть
+// объ€вление и реализаци€ шаблонного стека
+// стек поддерживает операции: 
+// - вставка элемента, +
+// - извлечение элемента, +
+// - просмотр верхнего элемента (без удалени€) +
+// - проверка на пустоту, +
+// - получение количества элементов в стеке +
+// - очистка стека +
+// при вставке в полный стек должна перевыдел€тьс€ пам€ть +
+#include <iostream>
+#pragma once
+using namespace std;
+
+template <class T>
+class TDynamicStack
+{
+    T* data;
+    size_t ind;
+    size_t capacity;
+    void resize()
+    {
+        T* newData = new T[capacity * 2]();
+        copy(data, data + capacity, newData);
+        capacity *= 2;
+        delete[] data;
+        data = newData;
+    }
+public:
+    TDynamicStack(size_t size = 10)
+    {
+        if (size == 0)
+        {
+            throw invalid_argument("Size should be greater than zero");
+        }
+        data = new T[size];
+        capacity = size;
+        ind = 0;
+    }
+    bool isEmpty() const noexcept
+    {
+        return ind == 0;
+    }
+    void clear()
+    {
+        if (capacity > 10)
+        {
+            T* newData = new T[10];
+            delete[] data;
+            data = newData;
+            capacity = 10;
+        }
+        ind = 0;
+    }
+    T top()
+    {
+        if (isEmpty())
+            throw exception("Stack is empty");
+        return data[ind - 1];
+    }
+    T pop()
+    {
+        if (isEmpty())
+            throw exception("Stack is empty");
+        else
+            return data[--ind];
+    }
+    void push(const T& e)
+    {
+        if (ind == capacity)
+            resize();
+        data[ind++] = e;
+    }
+    size_t size() const noexcept
+    {
+        return ind;
+    }
+
+    ~TDynamicStack()
+    {
+        delete[] data;
+        data = nullptr;
+        ind = 0;
+        capacity = 0;
+    }
+};
