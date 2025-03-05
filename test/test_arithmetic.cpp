624190823dc8a8c342c4378467eb660963e9c462
amelabrigid <121575401+amelabrigid@users.noreply.github.com>
2025-03-05 13:22:14 +0300

    Add files via upload

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,3 +1,257 @@
 // тесты для вычисления арифметических выражений
 
 #include <gtest.h>
+#include "arithmetic.h"
+
+TEST(Arithmetic, can_create_postfix_to_infix)
+{
+	string str = "x+y";
+	ASSERT_NO_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix)
+{
+	string str = "x+y";
+	string correct = "x y + ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, can_set_infix)
+{
+	string str = "x+y";
+	Arithmetic s(str);
+	string str1 = "x-y";
+	ASSERT_NO_THROW(s.setInfix(str1));
+}
+
+TEST(Arithmetic, correct_create_postfix_to_set_infix)
+{
+	string str = "x+y";
+	string str1 = "x-y";
+	string correct = "x y - ";
+	Arithmetic s(str);
+	s.setInfix(str1);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_unary_minus)
+{
+	string str = "-(x+(-y))";
+	string correct = "x y ~ + ~ ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_operation_with_different_priorities)
+{
+	string str = "-(x+y)*z";
+	string correct = "x y + ~ z * ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_long_variable_names)
+{
+	string str = "-(_xt+y_b)*z1k";
+	string correct = "_xt y_b + ~ z1k * ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_double)
+{
+	string str = "-(x+y)*8.9";
+	string correct = "x y + ~ 8.9 * ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_double_first_point)
+{
+	string str = "-(x+y)*.9";
+	string correct = "x y + ~ .9 * ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, correct_create_postfix_to_infix_with_double_with_e)
+{
+	string str = "-(x+y)*8.e-4";
+	string correct = "x y + ~ 8.e-4 * ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getPostfix());
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_space)
+{
+	string str = "-(x+y)* 8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_wrong_first_symbol)
+{
+	string str = "*(x+y)*8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_wrong_symbol)
+{
+	string str = "(x+y%)*8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_wrong_name_of_variable_or_index_without_mul)
+{
+	string str = "(x+5y)*8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_no_operation_between_breakets)
+{
+	string str = "(x+5y)(8.9*z)";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_two_operations_together)
+{
+	string str = "(x+-y)*8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_different_count_of_brackets)
+{
+	string str = "x+y)*(8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_wrong_first_brackets)
+{
+	string str = ")(x+y)*(8.9";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, cant_create_postfix_to_infix_with_wrong_last_symbol)
+{
+	string str = "(x+y)*8.9/";
+	ASSERT_ANY_THROW(Arithmetic s(str));
+}
+
+TEST(Arithmetic, correct_get_operands)
+{
+	string str = "x+y";
+	string correct = "x y ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getOperands());
+}
+
+TEST(Arithmetic, correct_get_long_operands)
+{
+	string str = "xn1+yn2";
+	string correct = "xn1 yn2 ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getOperands());
+}
+
+TEST(Arithmetic, correct_get_long_operands_with_the_same_first_symbol)
+{
+	string str = "xn1+yn2+xn2";
+	string correct = "xn1 yn2 xn2 ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getOperands());
+}
+
+TEST(Arithmetic, correct_get_operands_without_repeate)
+{
+	string str = "xn1+yn2*yn2";
+	string correct = "xn1 yn2 ";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getOperands());
+}
+
+TEST(Arithmetic, correct_get_infix)
+{
+	string str = "x+y";
+	string correct = "x+y";
+	Arithmetic s(str);
+	EXPECT_EQ(correct, s.getInfix());
+}
+
+TEST(Arithmetic, correct_make_double_from_string_with_dote)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 8.9;
+	EXPECT_EQ(doub, s.makeDouble("8.9"));
+}
+
+TEST(Arithmetic, correct_make_double_from_string_without_dote)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 89;
+	EXPECT_EQ(doub, s.makeDouble("89"));
+}
+
+TEST(Arithmetic, correct_make_double_from_string_without_end)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 8.;
+	EXPECT_EQ(doub, s.makeDouble("8."));
+}
+
+TEST(Arithmetic, correct_make_double_from_string_without_start)
+{
+	string str = "-(x+y)";
+	Arithmetic s(str);
+	double doub = .90;
+	EXPECT_EQ(doub, s.makeDouble(".90"));
+}
+
+TEST(Arithmetic, correct_make_double_from_string_with_e_positive)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 1.e+10;
+	EXPECT_EQ(doub, s.makeDouble("1.e10"));
+}
+
+TEST(Arithmetic, correct_make_double_from_string_with_e_negativ)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 4.e-2;
+	EXPECT_EQ(doub, s.makeDouble("4.e-2"));
+}
+
+TEST(Arithmetic, correct_make_negative_double_from_string_with_e_negativ)
+{
+	string str = "-(x+y)*8.9";
+	Arithmetic s(str);
+	double doub = 4.6e-2;
+	EXPECT_EQ(doub, s.makeDouble("4.6e-2"));
+}
+
+
+TEST(Arithmetic, correct_get_priorytets_of_operations)
+{
+	string str = "x+y";
+	Arithmetic s(str);
+	EXPECT_EQ(1, s.priority('+'));
+	EXPECT_EQ(1, s.priority('-'));
+	EXPECT_EQ(2, s.priority('*'));
+	EXPECT_EQ(2, s.priority('/'));
+}
+
+TEST(Arithmetic, correct_spot_user_operations)
+{
+	string str = "x+y";
+	Arithmetic s(str);
+	EXPECT_EQ(true, s.isOperation('+'));
+	EXPECT_EQ(true, s.isOperation('-'));
+	EXPECT_EQ(true, s.isOperation('*'));
+	EXPECT_EQ(true, s.isOperation('/'));
+	EXPECT_EQ(false, s.isOperation('f'));
+	EXPECT_EQ(false, s.isOperation('~'));
+}
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,104 @@
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(TDynamicStack, can_create_stack_with_positive_length)
+{
+	ASSERT_NO_THROW(TDynamicStack<int> s(5));
+}
+
+TEST(TDynamicStack, can_create_stack_without_length)
+{
+	ASSERT_NO_THROW(TDynamicStack<int> s());
+}
+
+TEST(TDynamicStack, cant_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(TDynamicStack<int> s(-5));
+}
+
+TEST(TDynamicStack, can_create_stack_with_null_length)
+{
+	ASSERT_ANY_THROW(TDynamicStack<int> s(0));
+}
+
+TEST(TDynamicStack, correct_see_empty_1)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	EXPECT_EQ(0,s.isEmpty());
+}
+
+TEST(TDynamicStack, correct_see_empty_2)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	s.pop();
+	EXPECT_EQ(1,s.isEmpty());
+}
+
+TEST(TDynamicStack, can_push_element)
+{
+	TDynamicStack<int> s(5);
+	ASSERT_NO_THROW(s.push(7));
+}
+
+TEST(TDynamicStack, can_push_and_resize_memory)
+{
+	TDynamicStack<int> s(1);
+	s.push(7);
+	ASSERT_NO_THROW(s.push(5));
+}
+
+TEST(TDynamicStack, can_pop_element)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	ASSERT_NO_THROW(s.pop());
+}
+
+TEST(TDynamicStack, cant_pop_empty_stack)
+{
+	TDynamicStack<int> s(5);
+	ASSERT_ANY_THROW(s.pop());
+}
+
+TEST(TDynamicStack, dont_delete_top_element)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	s.top();
+	EXPECT_EQ(0, s.isEmpty());
+	EXPECT_EQ(7, s.pop());
+}
+
+TEST(TDynamicStack, cant_see_top_in_empty_stack)
+{
+	TDynamicStack<int> s(5);
+	ASSERT_ANY_THROW(s.top());
+}
+
+TEST(TDynamicStack, can_clear_stack)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	s.push(5);
+	ASSERT_NO_THROW(s.clear(););
+}
+
+TEST(TDynamicStack, correct_clear_stack)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	s.push(5);
+	s.clear();
+	EXPECT_EQ(1, s.isEmpty());
+}
+
+TEST(TDynamicStack, correct_get_size)
+{
+	TDynamicStack<int> s(5);
+	s.push(7);
+	s.push(5);
+	EXPECT_EQ(2, s.size());
+}
